224b51bd73755156b13df9f1a9c575a4b4ab4c4c
D3XT3R404 <muhammaddavidfirmansyah01@gmail.com>
2026-04-22 00:41:35 +0700

    connect api railway ajjg berhasil cokaaodadh

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://d3xtinel-production.up.railway.app";
 
 const btn = document.getElementById("checkBtn");
 const urlInput = document.getElementById("urlInput");
